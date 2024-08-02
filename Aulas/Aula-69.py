cb90166be420a721035ff4f6abdada09165b31c2
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-02 20:20:31 -0300

    Aula69-1

---
+++ Aulas/Aula-69.py
@@ -0,0 +1,17 @@
+String = input("Digite algo: ")
+
+i = 0
+letra_a = 0
+while i < len(String):
+    
+    letra = String[i]
+
+    if letra == "a":
+
+        letra_a += 1
+        ˋsefweˋ
+        ''
+        ""
+    
+
+print(ˋ O texto (${String}) contem a letra (a) ${letra_a} vezes!ˋ)
