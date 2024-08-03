34096e0be7c248c3b33d32b5cc83a9dfb3c8e2ac
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-02 21:34:11 -0300

    Aula69-concluido

--- Aulas/Aula-69.py
+++ Aulas/Aula-69.py
@@ -2,16 +2,34 @@ String = input("Digite algo: ")
 
 i = 0
 letra_a = 0
+win = None
+
 while i < len(String):
     
     letra = String[i]
 
     if letra == "a":
-
         letra_a += 1
-        ˋsefweˋ
-        ''
-        ""
-    
 
-print(ˋ O texto (${String}) contem a letra (a) ${letra_a} vezes!ˋ)
+        
+        win = True
+    
+    if letra != "a" and i == len(String):
+        win = False
+        print(win)
+        
+
+    i+=1
+
+if win:
+        
+    la = str(letra_a)
+    texto = " O texto " + String + " contem a letra (a) "
+    print(texto + la + " vezes!" )
+
+else:
+
+    la = str(letra_a)
+    texto = "O texto " + String + " Não contem a letra (a)  "
+    print(texto)
+
