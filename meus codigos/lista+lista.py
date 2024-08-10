8eafb3875f167c61c39a26dd68008b901e6a42ad
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-10 21:26:59 +0000

    lista+listas

---
+++ meus codigos/lista+lista.py
@@ -0,0 +1,5 @@
+dados = [['jonatas', 'araujo', 27],['junio', 'elielson', 25],['jefinho', 'silva', 26],['vanderley', 'santana', 30]]
+
+print(dados)
+dados.append(['pinto', 'silva'])
+print(dados[1])
