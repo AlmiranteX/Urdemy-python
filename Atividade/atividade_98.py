11835b7fbdd5e85431b92c23757f55b916ca200d
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-22 22:09:17 -0300

    cpf

--- Atividade/atividade_98.py
+++ Atividade/atividade_98.py
@@ -2,9 +2,41 @@
 Atividade 98 Verificar CPF 
 """
 
-cpf = '088.171.595-65'
+cpf = '746.824.890-70'
 
-for i in range(len(cpf)):
+mult = 10
+registro = []
+soma = []
+resultado_soma = 0
+
+
+for i in cpf:
     
-    print(i)
+    if i == '.':
+        continue
+    if i == '-':
+        break
+    registro.append(int(i))
+    #print(registro)
+    
+for i in range(9):
+      
+    x = registro[i] * mult
+    soma.append(x)
+    print(f"{registro[i]}' X '{mult} = {x}")
+    mult -= 1
 
+for a in range(len(soma)):
+    resultado_soma += soma[a]
+    
+resultado_soma = resultado_soma * 10
+
+resultado = (resultado_soma % 11)
+
+if resultado > 9:
+    resultado = 0
+
+print(resultado, (resultado == registro[0]))
+        
+        
+    
