fee6ec4aac679a727d4dc8f133e9392bcdbb613d
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-23 22:45:46 -0300

    git

--- Atividade/atividade_98.py
+++ Atividade/atividade_98.py
@@ -4,47 +4,92 @@ Atividade 98 Verificar CPF
 
 cpf = '746.824.890-70'
 
-mult = 10
-registro = []
-soma = []
-resultado_soma = 0
 
-def algoritimo_cpf():
+def second_algoritimo(REgistro):
+    
+    mult = 11
+    resultado_soma = 0
+
+    for i in range(9):
+      
+        x = REgistro[i] * mult
+        soma.append(x)
+        print(f"{REgistro[i]}' X '{mult} = {x}")
+        mult -= 1
+    
+    resultado_soma = sum(soma)
+        
+    resultado_soma = resultado_soma * 10
+
+    resultado = (resultado_soma % 11)
+    
+    if resultado > 9:
+        resultado = 0
+ 
+        
+    if resultado == (int(cpf[-2]):)    
+         
+        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+        REgitro.append(resultado)
+        print(REgistro)
+        
+    else:
+        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
     
 
 
+def first_algoritimo(registro):
+    """_summary_
+        A funçao "first_algoritimo()" foi criada para validar o 1° digito do cpf.
+        a validação do digito e feita por meios de calculos baseado no algoritimo do cpf.
+    """
+    mult = 10
+    resultado_soma = 0
+    
+    for i in range(9):
+      
+        x = registro[i] * mult
+        soma.append(x)
+        print(f"{registro[i]}' X '{mult} = {x}")
+        mult -= 1
 
+    resultado_soma = sum(soma)
+        
+    resultado_soma = resultado_soma * 10
 
+    resultado = (resultado_soma % 11)
 
-
-
+    if resultado > 9:
+        resultado = 0
+ 
+        
+    if resultado == (int(cpf[-2]):)    
+         
+        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+        regitro.append(resultado)
+        print(registro)
+        second_algoritimo(registro)
+    else:
+        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+    
+    
+        
 for i in cpf:
     
     if i == '.':
         continue
     if i == '-':
         break
-    registro.append(int(i))
+    Registro.append(int(i))
+
+first_algoritimo(Registro)
  
     
-for i in range(9):
-      
-    x = registro[i] * mult
-    soma.append(x)
-    print(f"{registro[i]}' X '{mult} = {x}")
-    mult -= 1
 
 
-resultado_soma = sum(soma)
-    
-resultado_soma = resultado_soma * 10
 
-resultado = (resultado_soma % 11)
 
-if resultado > 9:
-    resultado = 0
 
-print(resultado, (resultado == int(cpf[-2])))
 
   
         
