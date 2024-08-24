e3a381812379cbdf85555b680b1164bdf86e4dd2
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-08-23 21:58:40 -0300

    git

--- Atividade/atividade_98.py
+++ Atividade/atividade_98.py
@@ -9,7 +9,6 @@ registro = []
 soma = []
 resultado_soma = 0
 
-<<<<<<< HEAD
 def algoritimo_cpf():
     
 
@@ -21,21 +20,12 @@ def algoritimo_cpf():
 
 for i in cpf:
     
-=======
-
-for i in cpf:
-    
->>>>>>> 5c42e0a9a22f74d053a26b050d6138f5dde97772
     if i == '.':
         continue
     if i == '-':
         break
     registro.append(int(i))
-<<<<<<< HEAD
  
-=======
-    #print(registro)
->>>>>>> 5c42e0a9a22f74d053a26b050d6138f5dde97772
     
 for i in range(9):
       
@@ -44,13 +34,8 @@ for i in range(9):
     print(f"{registro[i]}' X '{mult} = {x}")
     mult -= 1
 
-<<<<<<< HEAD
 
 resultado_soma = sum(soma)
-=======
-for a in range(len(soma)):
-    resultado_soma += soma[a]
->>>>>>> 5c42e0a9a22f74d053a26b050d6138f5dde97772
     
 resultado_soma = resultado_soma * 10
 
@@ -59,13 +44,8 @@ resultado = (resultado_soma % 11)
 if resultado > 9:
     resultado = 0
 
-<<<<<<< HEAD
 print(resultado, (resultado == int(cpf[-2])))
 
   
-=======
-print(resultado, (resultado == registro[0]))
-        
->>>>>>> 5c42e0a9a22f74d053a26b050d6138f5dde97772
         
     
