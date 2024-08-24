4c56a5d929313cb5a0e817f17cb828d4a7e831ed
AlmiranteX <99479251+AlmiranteX@users.noreply.github.com>
2024-08-24 19:40:49 -0300

    cpf-ok

--- Atividade/atividade_98.py
+++ Atividade/atividade_98.py
@@ -1,96 +1,102 @@
-"""
+""" 23/08/24
 Atividade 98 Verificar CPF 
 """
 
-cpf = '746.824.890-70'
-
+cpf = '088.171.959-65'
+registro = []
 
 def second_algoritimo(REgistro):
-    
-    mult = 11
-    resultado_soma = 0
-
-    for i in range(9):
-      
-        x = REgistro[i] * mult
-        soma.append(x)
-        print(f"{REgistro[i]}' X '{mult} = {x}")
-        mult -= 1
-    
-    resultado_soma = sum(soma)
         
-    resultado_soma = resultado_soma * 10
-
-    resultado = (resultado_soma % 11)
+    print(REgistro)
     
-    if resultado > 9:
-        resultado = 0
- 
-        
-    if resultado == (int(cpf[-2]):)    
-         
-        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
-        REgitro.append(resultado)
-        print(REgistro)
-        
-    else:
-        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
-    
-
-
-def first_algoritimo(registro):
-    """_summary_
-        A funçao "first_algoritimo()" foi criada para validar o 1° digito do cpf.
-        a validação do digito e feita por meios de calculos baseado no algoritimo do cpf.
-    """
     mult = 10
     resultado_soma = 0
-    
-    for i in range(9):
-      
-        x = registro[i] * mult
-        soma.append(x)
-        print(f"{registro[i]}' X '{mult} = {x}")
-        mult -= 1
+    soma = []
+    resultado = 0
+
+    """_summary_
+        A funçao "second_algoritimo()" foi criada para validar o 2° digito do cpf.
+        a validação do digito e feita por meios de calculos baseado no algoritimo do cpf.
+    """
+   
+
+        
+    for i in range(len(REgistro)):
+        
+            x = REgistro[i] * mult
+            soma.append(x)
+            print(f"{REgistro[i]}' X '{mult} = {x}")
+            mult -= 1
 
     resultado_soma = sum(soma)
-        
-    resultado_soma = resultado_soma * 10
-
-    resultado = (resultado_soma % 11)
-
+    
+    resultado = (resultado_soma % len(REgistro))
+    print(f"{resultado_soma} % 11 :resta {resultado}")
+    
+    
     if resultado > 9:
-        resultado = 0
- 
+           resultado = 0
         
-    if resultado == (int(cpf[-2]):)    
-         
-        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
-        regitro.append(resultado)
-        print(registro)
-        second_algoritimo(registro)
+   
+        
+    if resultado == int(cpf[-1]):    
+          
+            
+            print(f"O digito: {resultado} é {resultado == int(cpf[-1])}")
+            registro.append(resultado)
+            print(registro)
+           
     else:
-        print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
-    
-    
-        
+            registro.append(resultado)
+       
+            print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+            print(registro)
+            
 for i in cpf:
     
     if i == '.':
         continue
     if i == '-':
         break
-    Registro.append(int(i))
+    registro.append(int(i))
 
-first_algoritimo(Registro)
- 
-    
+def first_algoritimo():
+    """_summary_
+            A funçao "first_algoritimo()" foi criada para validar o 1° digito do cpf.
+            a validação do digito e feita por meios de calculos baseado no algoritimo do cpf.
+    """
+    mult = 10
+    resultado_soma = 0
+    soma = []
 
-
-
-
-
-
-  
         
-    
+    for i in range(len(registro)):
+        
+            x = registro[i] * mult
+            soma.append(x)
+            print(f"{registro[i]}' X '{mult} = {x}")
+            mult -= 1
+
+    resultado_soma = sum(soma)
+
+
+        
+    resultado = (resultado_soma % 11)
+
+    if resultado > 9:
+          resultado = 0
+
+        
+    if resultado == int(cpf[-2]):    
+        
+            
+            print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+            registro.append(resultado)
+        
+            second_algoritimo(registro)
+    else:
+            registro.append(resultado)
+        
+            print(f"O digito: {resultado} é {resultado == int(cpf[-2])}")
+           
+first_algoritimo()
