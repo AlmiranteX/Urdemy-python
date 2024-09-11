5466c5221d33d919744cde7fbc166de9dc9b88c1
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-09-11 09:35:53 -0300

    113-concluido

--- Atividade/atividade_113.py
+++ Atividade/atividade_113.py
@@ -4,3 +4,20 @@ nao nomeados
 
 e verificar se o resultado e impa ou par    
 """
+num = (1, 2, 3, 4, 5)
+
+def multiplicar(*numeros):
+    
+    n = 1
+
+    for i in numeros:
+        n *= i
+        print(n)
+
+    v = ("par")if int(n) % 2 == 0 else "impar"
+    
+    return v, n
+    
+resul = multiplicar(*num)
+
+print(*resul)
