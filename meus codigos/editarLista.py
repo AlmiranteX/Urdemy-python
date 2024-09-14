c64b04e339daf4e9326d79079dcf1e3ff2ddcf09
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-09-14 20:34:10 -0300

    ok

--- meus codigos/editarLista.py
+++ meus codigos/editarLista.py
@@ -1,5 +1,5 @@
 import os
-os.system('clear')
+os.system('cls')
 
 
 lista =  ['jonatas', 'araujo', 'aquino']
---
+++ meus codigos/livro_py/atv_2.3-2.8.py
@@ -0,0 +1,26 @@
+#__ Faça voce mesmo (2.3)
+print('ola Jonatas gostaria de aprender um pouco de python hoje ?')
+
+# faça voce mesmo (2.4)
+name = "jonatas araujo"
+name = name.lower()
+print(name)
+
+name = name.upper()
+print(name)
+
+name = name.title()
+
+print(name)
+
+#faça voce mesmo (2.5 e 2.6)
+citacao = 'Albest ainster disse uma vez "Uma pessoa que nunca cometeu um erro, nunca tentou algo novo"'
+print(citacao)
+
+#faça voce mesmo (2.6 ate 2.8)
+nomes = ' jonatas araujo de aquino anjo'
+nomes = nomes.strip()
+#nomes = nomes.removeprefix('anjo') Nao funcionou assim
+nomes = nomes.removesuffix("anjo")
+print(f'Correção do Nome:\n\t{nomes}')
+
