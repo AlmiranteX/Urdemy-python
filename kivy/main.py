a669e7551e51c49c2b2012da25be3f6bc6269e5f
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-09-15 20:53:45 -0300

    Aula kivy

--- kivy/main.py
+++ kivy/main.py
@@ -1,15 +1,2 @@
-# Importa o app, Builder
-# Criar o app
-# Criar a funcao Builder
-
-from kivy.app import App
-from kivy.lang import Builder
-
-tela = Builder.load_file("tela.kv")
-
-class MeuAplicativo(App):
-    def build(self):
-        return tela
-
-
-MeuAplicativo().run()
+# Importaçoes do kivy
+import kivy
