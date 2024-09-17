5b78050f1c45fb39191d4b7b5dcc51ec0e1c1f0e
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-09-17 20:21:04 -0300

    3.8-3.10

--- meus codigos/livro_py/atv_3.8-3.10.py
+++ meus codigos/livro_py/atv_3.8-3.10.py
@@ -1,3 +1,15 @@
 lugares = ['israel', 'eua', 'suica', 'japao', 'hawai']
 
-print(lugares)
+print(f"Lista nomal\n\t{lugares}")
+
+print(f"Lista sorted ordem alfabetica temporariamente\n\t{sorted(lugares)}")
+
+print(f"Lista sorted orden alfabetica alterada temporariamente\n\t{sorted(lugares, reverse=True)}")
+
+print(f"Lista original\n\t{lugares}")
+
+lugares.sort(reverse=True)
+print(f"Lista alterada orden alfabetica\n\t{lugares}")
+lugares.sort(reverse=False)
+print(f"Lista novamente alterada\n\t{lugares}")
+
