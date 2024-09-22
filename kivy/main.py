777b12186a2d8eb9a6f18370973ed682260d53d0
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-09-22 19:37:43 -0300

    kivy

--- kivy/main.py
+++ kivy/main.py
@@ -1,18 +1,22 @@
-# Importaçoes do kivy
-import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.config import Config
 
+# Define o tamanho da janela antes de carregar o App
+Config.set('graphics', 'width', '320')
+Config.set('graphics', 'height', '480')
 
-class FirstLayout(BoxLayout):
-    pass
-
-class test(App):
+# Define the main app class
+class SimpleApp(App):
     def build(self):
-        layer1 = FirstLayout()
-        layer1.add_widget(Button(text="First Kivy"))
-        return layer1
-    
-test().run()
+        return SimpleLayout()
 
+# Define the layout class
+class SimpleLayout(BoxLayout):
+    def on_BTN_1(self):
+        # Change the label's text when the button is pressed
+        self.ids.my_label.text = "Button clicked!"
+
+# Run the application
+if __name__ == "__main__":
+    SimpleApp().run()
