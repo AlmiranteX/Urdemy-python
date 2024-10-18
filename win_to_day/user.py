162cf1d493a7f59ccb0725612b013dd93255c918
AlmiranteX <araujojonatasapc152018@gmail.com>
2024-10-17 21:39:27 -0300

    up uaer

--- win_to_day/user.py
+++ win_to_day/user.py
@@ -1,6 +1,6 @@
 import requests, json
 
-link=('https://dia-de-pix-default-rtdb.firebaseio.com')
+LINK=('https://dia-de-pix-default-rtdb.firebaseio.com')
 
 class User:
     """_summary_
@@ -31,14 +31,71 @@ class User:
         """
         id_user = self.user
         id_password = self.password
-        
-        Users_userds = requests.get(f'{link}/dados/usuarios.json').json()
+        Users_userds = requests.get(f'{LINK}/dados/usuarios.json').json()
         
         if id_user in Users_userds.keys():
-            password=requests.get(f'{link}/usuarios/{id_user}/senha.json').json()
+            password=requests.get(f'{LINK}/usuarios/{id_user}/senha.json').json()
 
             if id_password ==  password:
                 return True
             return False
-        
         return False
+
+    def registre(self):
+        """_summary_
+        """
+        def send():
+            try:
+                #---Registrar usuario---#
+                url=(f'{LINK}/usuarios/{self.user}.json')
+                dados={
+                    'user':self.user,
+                    'name':self.name,
+                    'last_name':self.last_name,
+                    'age':self.age,
+                    'email':self.email,
+                    'telefone':self.phone,
+                    'password':self.password
+                }
+                response=requests.put(url, json=dados,)
+                
+                #registrar o user na pasta dados/usuarios
+                url = (f'{LINK}/dados/usuarios/{self.user}.json')
+                N_users=requests.get(f'{LINK}/dados/usuarios.json').json()
+                response=requests.put(url, json=len(N_users))
+                
+                #registrar o user na pasta dados/Ncompletos
+                url = (f'{LINK}/dados/Ncompletos/{self.name} {self.last_name}.json')
+                response=requests.put(url, json=self.user)
+                
+                #registrar o user na pasta dados/emails
+                email = self.email
+                email = email.removesuffix('@gmail.com')
+                url = (f'{LINK}/dados/emails/{email}.json')
+                response=requests.put(url, json=self.user)
+            
+                #registrar o user na pasta dados/telefones
+                url=(f'{LINK}/dados/telefones/{self.phone}.json')
+                response=requests.put(url, json=self.user)
+ 
+                return True
+            except:
+                return 'Erro de Envio..'
+
+        def verific():
+            dados = requests.get(f'{LINK}/dados.json').json()
+            
+            #verificar se dados nao existem
+            if self.user not in dados['usuarios']:
+                fullname = f'{self.name} {self.last_name}'
+                if fullname not in dados['Ncompletos']:
+                    if self.phone not in dados['telefones']:
+                        email = self.email.removesuffix('@gmail.com')
+                        if email not in dados['emails']:
+                            usuarios = requests.get(f'{LINK}/usuarios.json').json()
+                            usuarios = usuarios.keys()
+                            if self.user not in usuarios:
+                                send()
+                                return True
+            return False
+        
